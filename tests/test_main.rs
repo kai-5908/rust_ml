92f9c2acc5bcb5e04a2d656c32f6d50a3f95d740
kai-5908 <82012812+kai-5908@users.noreply.github.com>
2023-06-16 01:51:58 +0900

    fix workflow (#3)
    * fix workflow
    
    * add hyphen
    
    * fix
    
    * delete use cache file and fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * format
    
    * format
    
    * update readme
    
    * fix readme

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,11 +1,11 @@
 #[test]
 fn test_main() {
-    assert!(1 == 1);
+    assert_eq!(1, 1);
     assert_ne!(1, 2);
 }
 
 #[test]
 #[should_panic]
-fn test_panic_works(){
+fn test_panic_works() {
     panic!();
 }
