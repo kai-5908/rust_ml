5fd2fd28eb2842bae0d1b3635d44f267f69e2c7e
kai-5908 <dolce.cantabile.maestoso@gmail.com>
2023-06-19 02:25:55 +0900

    format

--- src/validate.rs
+++ src/validate.rs
@@ -1,29 +1,34 @@
 use polars::series::Series;
 
-
 pub fn validate_positive_float_from_series(float_series: &Series) {
-    let series_iter = float_series.f64().expect("series was not an f64 dtype").into_iter();
-    for val in series_iter{
+    let series_iter = float_series
+        .f64()
+        .expect("series was not an f64 dtype")
+        .into_iter();
+    for val in series_iter {
         match val {
             Some(val) if val > 0.0 => (),
-            _ => panic!("value is negative or null")
+            _ => panic!("value is negative or null"),
         }
     }
 }
 pub fn validate_sex_from_series(str_series: &Series) {
-    let series_iter = str_series.utf8().expect("series was not an utf8 dtype").into_iter();
-    for val in series_iter{
-        match val{
+    let series_iter = str_series
+        .utf8()
+        .expect("series was not an utf8 dtype")
+        .into_iter();
+    for val in series_iter {
+        match val {
             Some(val) if val == "MALE" || val == "FEMALE" => (),
-            _ => panic!("sex str value is not valid")
+            _ => panic!("sex str value is not valid"),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use polars::series::Series;
     use crate::validate::*;
+    use polars::series::Series;
     #[test]
     fn test_validate_positive_float_from_series_normal() {
         let sample_series: Series = [11.1, 22.2, 33.3].iter().collect();
